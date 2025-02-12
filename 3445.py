86e2b74c3ec2f6c3574c8ed36eadf1ec8e665aa3
jinyangdeng <jinyang@outlook.sg>
2025-02-13 01:38:13 +0800

    Add explanation

--- 3445.py
+++ 3445.py
@@ -1,4 +1,14 @@
+"""
+Original question: https://leetcode.com/problems/maximum-difference-between-even-and-odd-frequency-ii
+
+Kudos to: https://www.youtube.com/watch?v=9J3hrtjHBAg for explanation
+Interesting to transform the problem into kadane problem with sliding window. Originally, I implemented a deque buffer,
+ensuring that it is at least size k and contains >= 2 even chars and >= 1 odd char However, having such a deque will
+result in O(k) memory. Hence, by just tracking the left and right of the sliding window, I keep it to O(1) memory.
+"""
+
 from math import inf
+
 
 class Solution:
     def maxDifference(self, s: str, k: int) -> int:
@@ -24,10 +34,13 @@ class Solution:
                            and not (s[left] == evenChar and evenCharCount - leftEvenCharCount <= 2)):
                         leftOddCharCount += s[left] == oddChar
                         leftEvenCharCount += s[left] == evenChar
-                        helper[(leftOddCharCount % 2, leftEvenCharCount % 2)] = min(leftOddCharCount - leftEvenCharCount, helper.get((leftOddCharCount % 2, leftEvenCharCount % 2), inf))
+                        helper[(leftOddCharCount % 2, leftEvenCharCount % 2)] = min(
+                            leftOddCharCount - leftEvenCharCount,
+                            helper.get((leftOddCharCount % 2, leftEvenCharCount % 2), inf))
                         left += 1
 
-                    if (right - left >= k - 1) and (oddCharCount - leftOddCharCount >= 1) and (evenCharCount - leftEvenCharCount >= 2):
+                    if (right - left >= k - 1) and (oddCharCount - leftOddCharCount >= 1) and (
+                            evenCharCount - leftEvenCharCount >= 2):
                         oppositeKey = (1 - oddCharCount % 2, evenCharCount % 2)
                         if oppositeKey in helper:
                             res = max(res, oddCharCount - evenCharCount - helper[oppositeKey])
