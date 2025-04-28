ad41e645f033d404afd2205e6d9ea7a6c88839ad
jinyangdeng <jinyang@outlook.sg>
2025-04-28 23:20:48 +0800

    28 Apr 2025

---
+++ 3531.py
@@ -0,0 +1,18 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def countCoveredBuildings(self, n: int, buildings: List[List[int]]) -> int:
+        x_ranges = defaultdict(lambda: [float('inf'), float('-inf')])
+        y_ranges = defaultdict(lambda: [float('inf'), float('-inf')])
+
+        for x, y in buildings:
+            x_ranges[x][0] = min(x_ranges[x][0], y)
+            x_ranges[x][1] = max(x_ranges[x][1], y)
+            y_ranges[y][0] = min(y_ranges[y][0], x)
+            y_ranges[y][1] = max(y_ranges[y][1], x)
+
+        return sum(
+            x_ranges[x][0] < y < x_ranges[x][1] and y_ranges[y][0] < x < y_ranges[y][1]
+            for x, y in buildings
+        )
---
+++ 3532.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
+        parent = [i for i in range(n)]
+        rank = [0] * n
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        def union(x, y):
+            px = find(x)
+            py = find(y)
+            if rank[px] <= rank[py]:
+                parent[px] = py
+                rank[py] += 1
+            else:
+                parent[py] = px
+                rank[px] += 1        
+        
+
+        for i in range(1, len(nums)):
+            if nums[i] - nums[i - 1] <= maxDiff:
+                union(i, i - 1)
+        
+        return [find(u) == find(y) for u, y in queries]
+        
